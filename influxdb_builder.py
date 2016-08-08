57d74d6732cf05eec5008b543bf76371add3e996
Sebastien Chabrolles <s.chabrolles@fr.ibm.com>
2016-08-08 11:16:29 +0200

    Adding build packages function

--- influxdb_builder.py
+++ influxdb_builder.py
@@ -1,6 +1,6 @@
 #!/usr/bin/env python
 #
-# s.chabrolles@fr.ibm.com
+# V1.0: s.chabrolles@fr.ibm.com
 ##############################################################################
 
 import logging
@@ -9,6 +9,7 @@ import subprocess
 import os
 import sys
 import tarfile
+import fnmatch
 
 # GLOBAL VARIABLES definition
 HOME = os.path.dirname(os.path.realpath(__file__))
@@ -16,84 +17,118 @@ DOCKER_UBUNTU = "docker.io/schabrolles/ubuntu_ppc64le:latest"
 
 
 def main():
-        usage = "Usage: %prog [-d <container_name> | -p]"
-        parser = optparse.OptionParser(usage=usage)
+    if check_param():
 
-        parser.add_option(
-                "-d", "--docker",
-                dest="docker_container_name",
-                help="Build docker container with the following name"
-                )
-        parser.add_option(
-                "-p", "--packages",
-                action="store_true",
-                dest="build_packages_opt",
-                default=False,
-                help="Create packages (.deb, .rpm, .tar)"
-                )
-        parser.add_option(
-                "-b", "--branch",
-                dest="git_branch",
-                default="stable",
-                help="Choose the influxdb git branch (version) to compile. \
-                 (stable, beta, master, 0.13 ...)"
-                )
+        prepare_build()
 
-        debug = optparse.OptionGroup(
-                parser,
-                "debug",
-                "The following options are for debugging"
-                )
-        parser.add_option_group(debug)
+        if options.docker_container_name and options.build_packages_opt:
+            build_option = "all"
+        elif options.build_packages_opt:
+            build_option = "packages"
+        else:
+            build_option = "static"
 
-        debug.add_option(
-                "--loglevel",
-                dest="loglevel",
-                default="INFO",
-                help="Set logging level: CRITICAL, ERROR, WARNING, INFO,\
-                DEBUG (default: INFO)"
-                )
-        options, args = parser.parse_args()
+        logging.debug(
+            'Statting influxdb build with build_option = {}'
+            .format(build_option)
+            )
+        build_influxdb(build_option, options.git_branch)
 
-        ######################################################################
-        # Logging
+        if options.docker_container_name:
+            build_influxdb_container(options.docker_container_name)
 
-        numeric_level = getattr(logging, options.loglevel.upper(), None)
-        if not isinstance(numeric_level, int):
-                # raise ValueError('Invalid log level: %s' % options.loglevel)
-                logging.error('Invalid log level: %s' % options.loglevel)
-
-        logging.basicConfig(
-                # level=logging.INFO,
-                level=numeric_level,
-                format='%(asctime)s %(levelname)s %(message)s'
+            print ""
+            print " + Docker container generated:"
+            print "-------------------------------"
+            print run_check_output(
+                "docker images {}".format(options.docker_container_name),
+                allow_failure=True,
                 )
 
-        ######################################################################
-        # Checking parameters
+        if options.build_packages_opt:
+            logging.debug("chdir to {}".format(HOME))
+            os.chdir(HOME)
 
-        if not (options.docker_container_name or options.build_packages_opt):
-            parser.error(
-                "Must specify a build output type: packages \'-p\'"
-                "or docker_image \'-d <docker_image_name>\'"
+            output_dir = HOME + "/build_output/"
+            print ""
+            print " + InfluxDB packages generated are available here: "
+            print "----------------------------------------------------"
+            for package in ["rpm", "deb", "tar.gz"]:
+                print ""
+                print package + " package:"
+                for result in fnmatch.filter(
+                                            os.listdir(output_dir),
+                                            "*." + package):
+                    print output_dir + result
+    else:
+        sys.exit(1)
+
+
+def check_param():
+    logging.debug("Entering in check_param function")
+    if not (options.docker_container_name or options.build_packages_opt):
+        parser.error(
+            "Must specify a build output type: packages \'-p\'"
+            "or docker_image \'-d <docker_image_name>\'"
+            )
+        return False
+
+    elif options.docker_container_name and \
+            not options.docker_container_name[0].isalpha():
+        parser.error(
+            "Bad docker container name. It must start with an alpha"
+        )
+        return False
+
+    else:
+        return True
+
+
+def run_check_output(command, allow_failure=False, shell=False):
+    """Run shell command (convenience wrapper around subprocess).
+       Need to import subprocess
+       Need to import sys
+    """
+    out = None
+    logging.debug("{}".format(command))
+    try:
+        if shell:
+            out = subprocess.check_output(
+                command.split,
+                stderr=subprocess.STDOUT,
+                shell=shell
                 )
         else:
-            ###################################################################
-            ###################################################################
-            # BEGINIG OF REAL CODE
-            ###################################################################
-            prepare_build()
-
-            if options.docker_container_name and options.build_packages_opt:
-                build_option = "all"
-            elif options.build_packages_opt:
-                build_option = "packages"
-            else:
-                build_option = "static"
-
-            build_influxdb(options.git_branch)
-            if options.docker_container_name:
-                build_influxdb_container(options.docker_container_name)
+            out = subprocess.check_output(
+                command.split(),
+                stderr=subprocess.STDOUT
+                )
+        out = out.decode('utf-8').strip()
+        # logging.debug("Command output: {}".format(out))
+    except subprocess.CalledProcessError as e:
+        if allow_failure:
+            logging.warn(
+                "Command '{}' failed with error: {}".format(command, e.output)
+                )
+            return None
+        else:
+            logging.error(
+                "Command '{}' failed with error: {}".format(command, e.output)
+                )
+            sys.exit(1)
+    except OSError as e:
+        if allow_failure:
+            logging.warn(
+                "Command '{}' failed with error: {}".format(command, e)
+                )
+            return out
+        else:
+            logging.error(
+                "Command '{}' failed with error: {}".format(command, e)
+                )
+            sys.exit(1)
+    else:
+        return out
 
 
 def prepare_build():
@@ -140,15 +175,15 @@ def prepare_build():
     p.wait()
 
 
-def build_influxdb(branch):
+def build_influxdb(build_type, branch):
     """ Compile influxdb
     """
     docker_builder_cmd = "docker run --rm"\
         " -v {}/build_script:/build_script"\
         " -v {}/build_output:/build_output"\
         " --name influxdb_build  {}"\
-        " /build_script/build_influxdb.sh {}"\
-        .format(HOME, HOME, DOCKER_UBUNTU, branch)
+        " /build_script/build_influxdb.sh {} {}"\
+        .format(HOME, HOME, DOCKER_UBUNTU, build_type, branch)
 
     logging.info("Starting build in transient docker container")
     logging.debug("{}".format(docker_builder_cmd))
@@ -193,4 +228,62 @@ def build_influxdb_container(container_name):
 
 
 if __name__ == "__main__":
-        main()
+
+    usage = "Usage: %prog [-d <container_name> | -p] [options]"
+    parser = optparse.OptionParser(usage=usage)
+
+    parser.add_option(
+            "-d", "--docker",
+            dest="docker_container_name",
+            help="Build docker container with the following name"
+            )
+    parser.add_option(
+            "-p", "--packages",
+            action="store_true",
+            dest="build_packages_opt",
+            default=False,
+            help="Create packages (.deb, .rpm, .tar)"
+            )
+    parser.add_option(
+            "-b", "--branch",
+            dest="git_branch",
+            default="stable",
+            help="Choose the influxdb git branch (version) to compile. \
+             (stable, beta, master, 0.13 ...)"
+            )
+
+    debug = optparse.OptionGroup(
+            parser,
+            "debug",
+            "The following options are for debugging"
+            )
+    parser.add_option_group(debug)
+
+    debug.add_option(
+            "--loglevel",
+            dest="loglevel",
+            default="INFO",
+            help="Set logging level: CRITICAL, ERROR, WARNING, INFO,\
+            DEBUG (default: INFO)"
+            )
+    options, args = parser.parse_args()
+
+    ######################################################################
+    # Logging
+
+    numeric_level = getattr(logging, options.loglevel.upper(), None)
+    if not isinstance(numeric_level, int):
+            # raise ValueError('Invalid log level: %s' % options.loglevel)
+            logging.error('Invalid log level: %s' % options.loglevel)
+    log_format_func = '[%(levelname)s] %(funcName)s: %(message)s'
+    log_format_time = '%(asctime)s [%(levelname)s] %(message)s'
+    log_format_full = '%(asctime)s [%(levelname)s] %(asctime)s [%(levelname)s]'
+    logging.basicConfig(
+            # level=logging.INFO,
+            level=numeric_level,
+            format=log_format_func
+            )
+
+    ######################################################################
+    # Start Main
+    main()
